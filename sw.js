9dbe6deb72aa00c9a4a58e7b1fa42bdb1030d611
Anatoliy-UX <Internetpokypka24@gmail.com>
2026-08-16 14:53:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-cache-v2';
+const CACHE = 'budget-cache-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
